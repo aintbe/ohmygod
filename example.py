ba57df893ff9d569e7eb5fcb1cc4bbde2499acf5
Sori Lim <sori@chartmetric.com>
2024-11-05 19:50:19 -0800

    feat: create ohmygod package

---
+++ example.py
@@ -0,0 +1,25 @@
+from ohmygod import OhMyGod, HolyMessage
+import time
+
+omg = OhMyGod()
+
+@omg.protect("> Praying for a long running process")
+def long_running_process(fail: bool):
+    time.sleep(3)
+    if fail:
+        raise Exception("> Process failed")
+    else:
+        return "> Process succeeded"
+
+# Print a message and get user input
+key = omg.key("> Do you want to make it fail? (y/n): ")
+fail = key.lower() == "y"
+
+# Run the process and pray for success
+try:
+    res = long_running_process(fail)
+    omg.success(res)
+except Exception as e:
+    # Show an error message if the process fails
+    omg.clear()
+    omg.error(str(e))
